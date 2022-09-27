9914ccf954f91dcfa2cedc4b53a21c7cdd684d70
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-09-27 18:53:02 +0200

    add src/ folder

---
+++ src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
@@ -0,0 +1,8 @@
+package com.tiernoparla.ejercicioselementalesgf;
+
+public class EjerciciosElementalesGF {
+    
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }//MAIN
+} //EJERCICIOS ELEMENTALES
