9ee3e9f79ec078e19c81b31e924a71d9e41f4cfc
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-09-28 18:39:34 +0200

    fixing permisions

--- src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
+++ src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
@@ -1,12 +1,32 @@
 package com.tiernoparla.ejercicioselementalesgf;
 
 public class EjerciciosElementalesGF {
-    
+
     public static void main(String[] args) {
-       for (int i = 0; i <= 10; i++) {
-             if( i%2 == 0){
-            System.out.println(i);
- }
+        int i = 0;
+        while (i <= 10) {
+            i++;
+            if (i % 2 == 0) {
+                System.out.println(i);
+            } //IF
+        }//WHILE
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //   int[] num = {1,22,3,44,53,54,2,3,4,67,9,0,90};
+
+        //     for(int i = 0; i < num.length; i++){
+        //         System.out.println(num[i]);
+        //     }
+    }
+
 }
-    }//MAIN
-} //EJERCICIOS ELEMENTALES
+
+}
