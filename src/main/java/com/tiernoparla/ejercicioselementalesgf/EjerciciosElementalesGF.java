21396d597917d2853c386cf5333c639679e70d63
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-09-29 17:37:45 +0200

    Algoritmo division con input

--- src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
+++ src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
@@ -1,32 +1,50 @@
 package com.tiernoparla.ejercicioselementalesgf;
 
+import java.util.*;
+
 public class EjerciciosElementalesGF {
 
     public static void main(String[] args) {
-        int i = 0;
-        while (i <= 10) {
-            i++;
-            if (i % 2 == 0) {
-                System.out.println(i);
-            } //IF
-        }//WHILE
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //   int[] num = {1,22,3,44,53,54,2,3,4,67,9,0,90};
+        // EJERCICIO 1
+        // int i = 0;
+        // while (i <= 10) {
+        // i++;
+        // if (i % 2 == 0) {
+        // System.out.println(i);
+        // } //IF
+        // }//WHILE
 
-        //     for(int i = 0; i < num.length; i++){
-        //         System.out.println(num[i]);
-        //     }
+        //EJERCICIO 2
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            Integer N;
+            Integer D;
+            Integer R;
+            Integer Q;
+
+            System.out.println("Introduce N:");
+            N = entrada.nextInt();
+            System.out.println("Introduce D:");
+            D = entrada.nextInt();
+            System.out.println("R = " + N);
+            R = N;
+            Q = 0;
+
+            while (R >= D) {
+                R = R - D;
+                Q++;
+                System.out.println(R);
+            }
+                if((D*Q)+ R == N){
+                    System.out.println(true);
+                }
+
+
+           // System.out.println("R = " + R + "\n" + "Q = " + Q + "\n" + "D = " + D);
+           // System.out.println("( D * Q ) + R = ");
+           // System.out.println((D * Q) + R);
+        }
+
     }
-
-}
 
 }
