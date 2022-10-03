7b0fc48e9068b56e9ce03d7d73605842bf67cd48
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-10-03 17:26:43 +0200

    fibonacci 1

--- src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
+++ src/main/java/com/tiernoparla/ejercicioselementalesgf/EjerciciosElementalesGF.java
@@ -1,6 +1,6 @@
 package com.tiernoparla.ejercicioselementalesgf;
 
-import java.util.*;
+// import java.util.*;
 
 public class EjerciciosElementalesGF {
 
@@ -14,9 +14,9 @@ public class EjerciciosElementalesGF {
         // } //IF
         // }//WHILE
 
-        //EJERCICIO 2
+        // EJERCICIO 2
 
-        try (Scanner entrada = new Scanner(System.in)) {
+      /*  try (Scanner entrada = new Scanner(System.in)) {
             Integer N;
             Integer D;
             Integer R;
@@ -35,15 +35,33 @@ public class EjerciciosElementalesGF {
                 Q++;
                 System.out.println(R);
             }
-                if((D*Q)+ R == N){
-                    System.out.println(true);
-                }
+            if ((D * Q) + R == N) {
+                System.out.println(true);
+            }
 
-
-           // System.out.println("R = " + R + "\n" + "Q = " + Q + "\n" + "D = " + D);
-           // System.out.println("( D * Q ) + R = ");
-           // System.out.println((D * Q) + R);
+            // System.out.println("R = " + R + "\n" + "Q = " + Q + "\n" + "D = " + D);
+            // System.out.println("( D * Q ) + R = ");
+            // System.out.println((D * Q) + R);
         }
+        */
+
+        ///EJERCICIO3
+          //FIBONNACI
+             //ARRAYS
+    
+
+        int x1 = 1;
+        int x2 = 1;
+        int x3 = 0;
+
+        for(int i = 3; i<= 10; i++){
+            x3 = x2 + x1;
+            x1 = x2;
+            x2 = x3;
+            System.out.println(x3);
+        }
+      
+
 
     }
 
