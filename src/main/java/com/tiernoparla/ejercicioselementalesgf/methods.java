e76951014bfb87ccd4183453761cd131e375f7d4
Guillermo Figueras <willigamer120@gmail.com>
2022-10-26 14:26:49 +0200

    create file methods.java

---
+++ src/main/java/com/tiernoparla/ejercicioselementalesgf/methods.java
@@ -0,0 +1,11 @@
+package com.tiernoparla.ejercicioselementalesgf;
+
+public class methods {
+    public static void holaMundo(){
+        System.out.println("HOLA MUNDO");
+    }
+    public static void main(String[] args) {
+        holaMundo();
+    }
+    
+}
